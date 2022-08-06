072e4ffa57c0c9e1dacf412cffa00c3a66d31ad3
satinder <satindersingh472@gmail.com>
2022-08-06 15:14:05 -0600

    finished

---
+++ src/components/pageHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: `page-header`
+};
+</script>
+
+<style scoped>
+h1{
+    color:green;
+}
+</style>
